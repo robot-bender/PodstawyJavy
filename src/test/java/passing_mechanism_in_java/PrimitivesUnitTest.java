4140d7d52344015473fd0f59a9a6649feef77aee
kjaniak <krystian.janiak@capgemini.com>
2022-07-22 20:40:17 +0200

    passing_mechanism_in_java

---
+++ src/test/java/passing_mechanism_in_java/PrimitivesUnitTest.java
@@ -0,0 +1,31 @@
+package passing_mechanism_in_java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class PrimitivesUnitTest {
+
+    @Test
+    public void whenModifyingPrimitives_thenOriginalValuesNotModified() {
+
+        int x = 1;
+        int y = 2;
+
+        //before modification
+        assertEquals(x,1);
+        assertEquals(y,2);
+
+        modify(x,y);
+
+        //after modification
+        assertEquals(x,1);
+        assertEquals(y,2);
+    }
+
+    private void modify(int x1, int y1) {
+        x1 = 5;
+        y1 = 10;
+    }
+}
